eb458ba30e0a3cbecdfff3ab9442d69258c0cf9b
RumorTv <62171023+RumorTv@users.noreply.github.com>
2020-05-03 16:15:14 +0530

    robofriend change

---
+++ robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+{
+	id:0,
+	name: 'Leane ',
+	username:'lreat',
+	email: 'sincdr@gmail.com'
+},
+{
+	id:1,
+	name: 'Lne ',
+	username:'eat',
+	email: 'sinc@gmail.com'
+},
+{
+	id:2,
+	name: 'Lnsdfe ',
+	username:'Brsadfeat',
+	email: 'sdsfincdr@gmail.com'
+},
+{
+	id:3,
+	name: 'Leanedfas ',
+	username:'Breatdsf',
+	email: 'sincdr@gmadsfsail.com'
+},
+{
+	id:4,
+	name: 'Ldfdsaeane ',
+	username:'Bresdfsaat',
+	email: 'sincdr@gmadsafil.com'
+},
+{
+	id:5,
+	name: 'Leaneasdf ',
+	username:'Breatdsf',
+	email: 'sincdr@gmadsfil.com'
+},
+]
